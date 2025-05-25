713a43043f9ca8819fa93ac640dedca0837e4712
Niclas Frank <a@bcom>
2025-05-25 10:58:39 +0200

    generator namespace

--- ConsoleAppTest/Generator.cs
+++ ConsoleAppTest/Generator.cs
@@ -1,39 +1,40 @@
 using System;
 
-namespace MathProblems;
-
-
-
-
-public class Generator
+namespace MathProblems
 {
-	[Flags]
-	public enum Types {
-	Addition,
-	Multiplication,
-	Division
-}
-
-	private static Random randy = new Random();
-
-
-	public static string[] getProblem(Types types)
+	public class Generator
 	{
-		//problem / solution
-		string[] result = { "", "" };
-		float solution = 0;
-
-		if ((types & Types.Addition) == Types.Addition)
-		{
-			int a = randy.Next(10);
-			int b = randy.Next(10);
-			result[0] += a + "+" + b;
-			solution = a + b;
-		}
-
-
-		result[1] = solution.ToString();
-		return result;
+		[Flags]
+		public enum Types {
+		Addition,
+		Multiplication,
+		Division
 	}
 
+		private static Random randy = new Random();
+
+
+		public static string[] getProblem(Types types)
+		{
+			//problem / solution
+			string[] result = { "", "" };
+			float solution = 0;
+
+			if ((types & Types.Addition) == Types.Addition)
+			{
+				int a = randy.Next(10);
+				int b = randy.Next(10);
+				result[0] += a + "+" + b;
+				solution = a + b;
+			}
+
+
+			result[1] = solution.ToString();
+			return result;
+		}
+
+	}
 }
+
+
+
